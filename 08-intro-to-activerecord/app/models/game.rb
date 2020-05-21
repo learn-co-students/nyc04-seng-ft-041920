3d84e39aab3bfebb981c2f75428e373f589f3163
ihollander <ianholla@gmail.com>
2020-05-21 11:37:18 -0400

    after intro to ar

--- 08-intro-to-activerecord/app/models/game.rb
+++ 08-intro-to-activerecord/app/models/game.rb
@@ -15,6 +15,7 @@ class Game < ActiveRecord::Base
   # Update
   # Game#save -> updates the instance in the database if it has an id
   # Game#update(hash) -> updates all column in the has for that instance
+  # Game.update(hash) -> update all the instances
 
   # Delete
   # Game#destroy -> delete the instance from the database
