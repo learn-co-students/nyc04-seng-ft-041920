e96fbd34fc02ca96c9886cf616dd3011f7ce516b
Michelle Rios <brendamichelle995@gmail.com>
2020-05-29 15:59:55 -0400

    after lecture

--- 09-hashes-and-the-internet/run.rb
+++ 09-hashes-and-the-internet/run.rb
@@ -3,17 +3,6 @@ require 'rest-client'
 require 'json'
 
 
-
-
-# make a request
-# https://www.googleapis.com/books/v1/volumes?q=old+man+and+the+sea
-
-
-
-# response = RestClient.get('https://www.googleapis.com/books/v1/volumes?q=old+man+and+the+sea')
-# binding.pry
-# play around with the data we get back 
-
 def welcome
     puts "Welcome to the Books finder! Please enter the type of books you'd like to see!"
 end
@@ -30,8 +19,10 @@ end
 def run
     welcome
     user_input = get_user_input
-    books = get_books(user_input)
-    binding.pry
+
+    get_books(user_input)["items"].each do |item|
+       puts item["volumeInfo"]["title"]
+    end
 end
 
 run
