0a52b7eb00f87da8d33d17d1f7d14e60bf151bcd
Michelle Rios <brendamichelle995@gmail.com>
2020-06-08 16:37:32 -0400

    after rails forms part 2

--- 15-rails-forms/pry-hotels/app/controllers/hotels_controller.rb
+++ 15-rails-forms/pry-hotels/app/controllers/hotels_controller.rb
@@ -1,27 +1,31 @@
 class HotelsController < ApplicationController
     
+    # get "/hotels"
     def index
         @hotels = Hotel.all
         render :index
     end
 
+    # get "/hotels/:id"
     def show
         @hotel = Hotel.find(params[:id])
         render :show
     end
 
+    # get "/hotels/new"
     def new
         @hotel = Hotel.new
         render :new
     end
     
+    # get "/hotels/:id/edit"
     def edit
         @hotel = Hotel.find(params[:id])
         render :edit
     end
 
+    # patch "/hotels/:id" 
     def update
-        byebug
         hotel = Hotel.find(params[:id])
         hotel_params = params.require(:hotel).permit(:name, :rating, :dining, :amenities)
         hotel.update(hotel_params)
@@ -29,22 +33,23 @@ class HotelsController < ApplicationController
         redirect_to hotel_path(hotel)
     end
 
+    # post "/hotels" 
     def create
-        # byebug
         # hotel_params = params.require(:hotel).permit!
-        # The above line will allow us to do mass assignment, but it will not give 
+        # The line above will allow us to do mass assignment, but it will not give 
         # us the added security that we would get writing it as the line below
+
         hotel_params = params.require(:hotel).permit(:name, :rating, :dining, :amenities, :location)
         hotel = Hotel.create(hotel_params)
+
         redirect_to hotel_path(hotel)
     end
 
+    # delete "/hotels/:id"
     def destroy
-        # find the model
         hotel = Hotel.find(params[:id])
-        # destroy it
         hotel.destroy
-        # redirect
+
         redirect_to hotels_path
     end
 
