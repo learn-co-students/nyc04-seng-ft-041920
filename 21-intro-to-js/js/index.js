50fe448ad949900cd46c75df99bac25ad3ede584
ihollander <ianholla@gmail.com>
2020-06-22 09:29:39 -0400

    before intro to js

---
+++ 21-intro-to-js/js/index.js
@@ -0,0 +1,20 @@
+console.log('HELLO WORLD')
+console.log('%cHELLO WORLD', 'color: blue')
+
+debugger
+
+let aCoolVariable = "😎"
+
+/**** data types ****/
+
+/**** truthy/falsey values ****/
+
+/*** strict equality vs loose equality ***/
+
+/**** pass by reference vs pass by value ****/
+
+/**** arrays and objects ****/
+
+/**** function definition vs invocation ****/
+
+/**** arrays & callbacks ****/
