848ab59f7e1fbe17e0b004a564681072470bd5e1
ihollander <ianholla@gmail.com>
2020-06-22 14:31:45 -0400

    before dom

---
+++ 22-dom-manipulation/airbnb/index.js
@@ -0,0 +1,51 @@
+// CRUD with the DOM
+
+// Read
+
+// Update
+
+// Delete
+
+// Create
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO: create cards for each object in this array
+const data = [
+  {
+    imageUrl: "https://i.kinja-img.com/gawker-media/image/upload/s--vYNPTkDt--/c_scale,fl_progressive,q_80,w_800/198qdy1m0jf7mjpg.jpg",
+    location: "Middle Earth",
+    price: 400,
+    title: "Upscale Lair in Quiet Neighborhood",
+    rating: 4.23
+  },
+  {
+    imageUrl: "https://66.media.tumblr.com/184f1c4149e8618763346cc0fe6acc37/tumblr_mvx9ut2WEe1sh566ao1_1280.jpg",
+    location: "Winterfell",
+    price: 200,
+    title: "Historic Castle in 'Cool' Uptown District",
+    rating: 2.71
+  },
+  {
+    imageUrl: "https://thecollegepost.com/wp-content/uploads/2019/12/main-building-750x430.jpg",
+    location: "Philadelphia",
+    price: 123,
+    title: "Student-Friendly Downtown Abode",
+    rating: 3.46
+  }
+]
