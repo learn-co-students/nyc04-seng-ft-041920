4a41437cf166c9f34b61fd87eefc204aa0a8c2f6
ihollander <ianholla@gmail.com>
2020-07-06 09:06:08 -0400

    before this lecture

---
+++ 30-this-and-execution-context/index.js
@@ -0,0 +1,13 @@
+function whatsThis() {
+  console.log("THIS IS:", this)
+}
+
+const dog1 = {
+  firstName: 'fezzik',
+  favSnacks: ['peanut butter', 'soccer balls'],
+}
+
+const dog2 = {
+  name: 'lucy',
+  favSnacks: ['carrots', 'french fries'],
+}
